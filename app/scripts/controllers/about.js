92d2548810b9e8101c54584d96d8b87355b0b7de
ariffurqon <arif.furqon@gmail.com>
2015-09-08 15:53:04 -0700

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fashionWeekApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the fashionWeekApp
+ */
+angular.module('fashionWeekApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fashionWeekApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fashionWeekApp
+ */
+angular.module('fashionWeekApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
